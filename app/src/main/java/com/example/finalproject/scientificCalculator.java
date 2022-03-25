1c4449358baef5df84f754f9fd46a31500f4a4b8
YT65a <65a.business@gmail.com>
2022-03-25 12:02:17 -0500

    added scientific calculator page designs

---
+++ app/src/main/java/com/example/finalproject/scientificCalculator.java
@@ -0,0 +1,21 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class scientificCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scientific_calculator);
+    }
+
+    public void homePage(View v){
+        Intent homePage = new Intent(this, conversionHome.class);
+        startActivity(homePage);
+    }
+}
