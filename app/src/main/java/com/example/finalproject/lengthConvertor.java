3e3db293efe17de20546dc5164608456c3f45231
YT65a <65a.business@gmail.com>
2022-04-18 11:06:14 -0500

    we did something but it failed :)

--- app/src/main/java/com/example/finalproject/lengthConvertor.java
+++ app/src/main/java/com/example/finalproject/lengthConvertor.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,42 +25,60 @@ public class lengthConvertor extends AppCompatActivity {
 
     private Spinner spinner1, spinner2;
     private Button btnSubmit;
+    private Spinner inputUnit, outputUnit;
+    private double input, output,result;
+    private EditText outputResult, inputNumber;
+    private String finalAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_length_convertor);
-        addListenerOnButton();
-
-//        spinner = (Spinner)findViewById(R.id.spinner);
-//        ArrayAdapter<String>adapter = new ArrayAdapter<String>(lengthConvertor.this,
-//                android.R.layout.simple_spinner_item,paths);
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-//        spinner.setOnItemSelectedListener(this);
     }
 
-    // add items into spinner dynamically
+    public void convert(View v) {
+        inputUnit = findViewById(R.id.spinner1);
+        outputUnit = findViewById(R.id.spinner2);
 
-    // get the selected dropdown list value
-    public void addListenerOnButton() {
+        inputNumber = (EditText) findViewById(R.id.input);
+        outputResult = (EditText) findViewById(R.id.outputresult);
+        double input = Double.parseDouble(inputNumber.toString());
 
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        String inputText = inputUnit.getSelectedItem().toString();
+        String outputText = outputUnit.getSelectedItem().toString();
 
-        btnSubmit.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(lengthConvertor.this,
-                        "OnClickListener : " +
-                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
-                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
-            }
-
-        });
+        if(inputText.equals("mm")){
+           if(outputText.equals("cm")) {
+               result = input * 10;
+               finalAnswer = String.format("%.3f", result);
+               outputResult.setText(finalAnswer);
+           }
+        }
+//        else if(inputText == "mm" && outputText == "mm"){
+//            result = input * 1000;
+//            finalAnswer = String.format("%.3f", result);
+//            outputResult.setText(finalAnswer);
+//        }
+//        else if(inputText == "mm" && outputText == "km"){
+//            result = input * 1000000;
+//            finalAnswer = String.format("%.3f", result);
+//            outputResult.setText(finalAnswer);
+//        }
+//        else if(inputText == "mm" && outputText == "in"){
+//            result = input / 25.4;
+//            finalAnswer = String.format("%.3f", result);
+//            outputResult.setText(finalAnswer);
+//        }
+//        else if(inputText == "mm" && outputText == "ft"){
+//            result = input / 305;
+//            finalAnswer = String.format("%.3f", result);
+//            outputResult.setText(finalAnswer);
+//        }
+//        else if(inputText == "mm" && outputText == "mi"){
+//            result = input / 1852000;
+//            finalAnswer = String.format("%.3f", result);
+//            outputResult.setText(finalAnswer);
+//        }
     }
 
     public void homePage(View v){
