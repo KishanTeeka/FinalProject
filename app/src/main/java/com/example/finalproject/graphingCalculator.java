907202b22326bc22659a8356bc9f810681d550fb
YT65a <65a.business@gmail.com>
2022-04-27 10:31:09 -0500

    ...

--- app/src/main/java/com/example/finalproject/graphingCalculator.java
+++ app/src/main/java/com/example/finalproject/graphingCalculator.java
@@ -1,20 +1,28 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+import java.lang.Math;
+import java.util.Vector;
+import java.util.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class graphingCalculator extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphing_calculator);
     }
-
-    public void homePage(View v){
+    
+    public void homePage(View v) {
         Intent homePage = new Intent(this, MainActivity.class);
         startActivity(homePage);
     }
